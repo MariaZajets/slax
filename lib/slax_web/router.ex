725f07bbcbe00f2951fae8578c49fc713d024e5a
MariaZajets <mariazaets3@gmail.com>
2025-06-17 19:33:56 +0300

    adjust auth code

--- lib/slax_web/router.ex
+++ lib/slax_web/router.ex
@@ -18,13 +18,6 @@ defmodule SlaxWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", SlaxWeb do
-    pipe_through :browser
-
-    live "/", ChatRoomLive
-    live "/rooms/:id", ChatRoomLive
-    live "/rooms/:id/edit", ChatRoomLive.Edit
-  end
 
   # Other scopes may use custom stacks.
   # scope "/api", SlaxWeb do
@@ -69,6 +62,9 @@ defmodule SlaxWeb.Router do
 
     live_session :require_authenticated_user,
       on_mount: [{SlaxWeb.UserAuth, :ensure_authenticated}] do
+      live "/", ChatRoomLive
+      live "/rooms/:id", ChatRoomLive
+      live "/rooms/:id/edit", ChatRoomLive.Edit
       live "/users/settings", UserSettingsLive, :edit
       live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
     end
